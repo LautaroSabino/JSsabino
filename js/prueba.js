b8fd13204dad3307e70ba87b08cd5a118dec76e4
Lautaro Sabino <lautarosabino26@gmail.com>
2022-07-22 15:19:15 -0300

    inicio de repositorio

---
+++ js/prueba.js
@@ -0,0 +1,27 @@
+//esta es la forma de comentar, con las dos barras, en una sola línea.
+
+/*esta es la forma de comentar
+en varias líneas*/
+
+//PRUEBA DE ALGORITMO SIMPLE
+
+/*let username = prompt("Ingrese su nombre: ");
+let userLastname = prompt("Ingrese su apellido: ");
+let userAge = parseInt(prompt("Ingrese su edad: ")); 
+
+let resultado = username + " " + userLastname + " " + userAge;
+
+alert(resultado); */
+
+//PRUEBA DE CONDICIONAL SIMPLE, BÁSICA
+
+// let edad = parseInt(prompt("Ingrese su edad: "));
+
+// if (edad >= 18) {
+//     alert("Usted puede ingresar y hacerse una cuenta");
+
+// } else if(edad >= 13){
+//     alert("Usted puede crearse una cuenta con restricciones");
+// } else{
+//     alert("Usted de ninguna manera puede hacerse una cuenta, váyase");
+// }
