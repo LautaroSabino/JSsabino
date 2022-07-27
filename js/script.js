48bb027236b03114693e812818b166e4b25b77f9
Lautaro Sabino <lautarosabino26@gmail.com>
2022-07-27 13:52:08 -0300

    sigo con estructura de primer entregable para proyecto final

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+alert("Buenos días, viajero/a. Hoy vas a ingresar a un nuevo mundo de aventuras. Mi nombre es Jhon Montalto y mi misión es ayudarte en este viaje. ¿Cómo es tu nombre? ");
+
+let nombre = prompt("Por favor, ingresá tu nombre: ");
+
 function saludar(){
-    let saludo
+    alert("Bienvenido/a " + nombre + " a este nuevo mundo!");
 }
+
+saludar();
+
+alert("Ahora, necesito que me digas un número del 1 al 10, el que más te guste: ");
+
+let numero = parseInt(prompt("Ingresa un número del 1 al 10"));
+
+alert("Excelente, tú oro inicial será de:  " + numero + "  monedas.");
