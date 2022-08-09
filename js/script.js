f2e4581c0d3840ca238465aa48facac647444a89
Lautaro Sabino <lautarosabino26@gmail.com>
2022-08-09 13:42:46 -0300

    uso de funciones de orden superior

--- js/script.js
+++ js/script.js
@@ -21,8 +21,13 @@ const pizzaneza = new Menu('Pizzaneza', 2450, '4 personas');
 const pizzaMuzza = new Menu('Pizza muzzarella', 1100, '3 personas');
 const omelet = new Menu('Omelet de jamón y queso', 900, '1 persona');
 const ravioles = new Menu('Ravioles', 800, '1 persona');
-const chocotorta = new Menu('Chocotorta', 550, '2 personas');
+const chocotorta = new Menu('Chocotorta', 500, '2 personas');
 const pizzaEsp = new Menu('Pizza especial', 1500, '3 personas');
+const pizzaNapo = new Menu('Pizza napolitana', 1500, '3 personas');
+const pizzaRoque = new Menu('Pizza roquefort', 1800, '3 personas');
+const pizzaCala = new Menu('Pizza calabreza', 1900, '3 personas');
+
+
 
 omelet.sumarIva();
 console.log("El precio de " + omelet.comida + " es " + omelet.precio );
@@ -34,12 +39,16 @@ const pastas = [lasagna, ravioles];
 const postres = [budin, chocotorta];
 const pizzas = [pizzaMuzza, pizzaEsp];
 
-for (let i of minutas) {
+
+for (let i in minutas) {
     console.log(i);
+    console.log(minutas[i]);
 }
+
 
 for (let i in especialidades) {
     console.log(i);
+    console.log(especialidades[i]);
 }
 
 pizzas.push("pizzaNapo", "pizzaRoquefot");
@@ -48,8 +57,29 @@ console.log(pizzas);
 pizzas.unshift("pizzaCalabresa");
 console.log(pizzas, pizzas.length);
 
-pizzas.pop;
-console.log(pizzas);
-
 pastas.join(" ,");
 console.log(pastas);
+
+const encontrado = minutas.find((minutas)=> minutas.precio < 500);
+console.log(encontrado);
+
+const filtrado = pizzas.filter((pizzas)=> pizzas.precio > 1000);
+console.log(filtrado);
+
+const filtradoDos = pizzas.filter((pizzas)=> pizzas.precio > 2000);
+console.log(filtradoDos);
+
+const buscaPostre = postres.some((postres)=> postres.precio < 600);
+console.log(buscaPostre);
+
+const descuento = especialidades.map((especialidades)=>{
+    return{
+        comida: especialidades.comida,
+        precio: especialidades.precio*.90
+    }
+})
+
+console.log(descuento);
+
+
+
