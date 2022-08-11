149a06b327dfbe5398f7a5292131d5f90177e2b8
Lautaro Sabino <lautarosabino26@gmail.com>
2022-08-11 16:06:24 -0300

    eventos

--- js/script.js
+++ js/script.js
@@ -11,6 +11,8 @@ class Menu {
     }
 }
 
+/*-----------------------------------------------------------------*/
+
 const pollo = new Menu('Pollo al espiedo', 1700, '3 personas');
 const tarta = new Menu('Tarta de verdura', 1100, '2 personas');
 const empanada = new Menu('Empanada de carne', 160, '1 persona');
@@ -27,11 +29,15 @@ const pizzaNapo = new Menu('Pizza napolitana', 1500, '3 personas');
 const pizzaRoque = new Menu('Pizza roquefort', 1800, '3 personas');
 const pizzaCala = new Menu('Pizza calabreza', 1900, '3 personas');
 
+/*-------------------------------------------------------------------*/
+
 const especialidades = [pollo, guiso, omelet];
 const minutas = [tarta, empanada, pizzaneza];
 const pastas = [lasagna, ravioles];
 const pizzas = [pizzaMuzza, pizzaEsp];
 const postres = [budin, chocotorta];
+
+/*---------------------------------------------------------------------*/
 
 omelet.sumarIva();
 console.log(`El precio de ${omelet.comida} es ${omelet.precio}` );
@@ -44,6 +50,8 @@ console.log(pizzas, pizzas.length);
 
 pastas.join(" ,");
 console.log(pastas);
+
+// /*------------------------------------------------------------------*/
 
 const encontrado = minutas.find((minutas)=> minutas.precio < 500);
 console.log(encontrado);
@@ -63,33 +71,62 @@ const descuento = especialidades.map((especialidades)=>{
 
 console.log(descuento);
 
-let saludo = prompt(`Bienvenido a Don Marino. ¿Qué menú deseas ver el día de hoy?
-\n Por favor, ingresá la opción correspondiente:
-\n 1- ESPECIALIDADES, 2- MINUTAS, 3- PASTAS, 4- PIZZAS , 5-POSTRES`);
+/*-----------------------------------------------------------------*/
 
-saludo = parseInt(saludo);
+const container = document.querySelector('#muestra');
 
-if (saludo == 1) {
-    especialidades.forEach(element => {
-        console.log(element)
-    });
+let input = document.querySelector('#input');
+let button = document.querySelector('#button');
 
-} else if (saludo == 2) {
-    minutas.forEach(element => {
-        console.log(element)
-    });
-} else if (saludo == 3) {
-    pastas.forEach(element => {
-        console.log(element)
-    });
-} else if (saludo == 4) {
-    pizzas.forEach(element => {
-        console.log(element)
-    });
-} else if (saludo == 5) {
-    postres.forEach(element => {
-        console.log(element)
-    });
-} else {
-    console.log('Lo siento, no ingresaste el dato correspondiente');
+const handleClick = () => {
+    
+    let item = document.createElement('p');
+    item.innerText = input.value;
+
+    container.appendChild(item)
+    input.value = '';
 }
+
+button.addEventListener('click', handleClick)
+
+// const  buscar  =  documento . getElementById ( "main" )
+// let nuevoDiv = document.createElement(`div`);
+// nuevoDiv.innerHTML = `<h1> Bienvenido a Don Marino </h1> `;
+// nuevoDiv.className = `divJs`
+// console.log(nuevoDiv);
+
+/*-----------------------------------------------------------------*/
+
+// let saludo = prompt(`Bienvenido a Don Marino. ¿Qué menú deseas ver el día de hoy?
+// \n Por favor, ingresá la opción correspondiente:
+// \n 1- ESPECIALIDADES, 2- MINUTAS, 3- PASTAS, 4- PIZZAS , 5-POSTRES`);
+
+// saludo = parseInt(saludo);
+
+// if (saludo == 1) {
+//     especialidades.forEach(element => {
+//         console.log(element)
+//     });
+
+// } else if (saludo == 2) {
+//     minutas.forEach(element => {
+//         console.log(element)
+//     });
+// } else if (saludo == 3) {
+//     pastas.forEach(element => {
+//         console.log(element)
+//     });
+// } else if (saludo == 4) {
+//     pizzas.forEach(element => {
+//         console.log(element)
+//     });
+// } else if (saludo == 5) {
+//     postres.forEach(element => {
+//         console.log(element)
+//     });
+// } else {
+//     console.log('Lo siento, no ingresaste el dato correspondiente');
+// }
+
+/*-----------------------------------------------------------------*/
+
