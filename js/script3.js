23fa655206bce67e526d1be69914299ed9835e25
Lautaro Sabino <lautarosabino26@gmail.com>
2022-09-06 15:51:23 -0300

    mejora para el settimeout

--- js/script3.js
+++ js/script3.js
@@ -1,17 +1,8 @@
-const botonTarjeta1 = document.querySelector('#btnTarjeta1');
-const botonTarjeta2 = document.querySelector('#btnTarjeta2');
-const botonTarjeta3 = document.querySelector('#btnTarjeta3');
-const botonTarjeta4 = document.querySelector('#btnTarjeta4');
-const botonTarjeta5 = document.querySelector('#btnTarjeta5');
-const botonTarjeta6 = document.querySelector('#btnTarjeta6');
+const btnTar1 = document.querySelector('#btnTar1');
 
-const muestraTarjeta1 = document.getElementById('#tarjeta1');
-const muestraTarjeta2 = document.querySelector('.tarjeta2');
-const muestraTarjeta3 = document.querySelector('.tarjeta3');
-const muestraTarjeta4 = document.querySelector('.tarjeta4');
-const muestraTarjeta5 = document.querySelector('.tarjeta5');
-const muestraTarjeta6 = document.querySelector('.tarjeta6');
+const tar1 = document.querySelector('.tar1');
 
-botonTarjeta1.addEventListener('click', ()=>{
-    muestraTarjeta1.classList.remove('.invisible')
+
+btnTar1.addEventListener('click', () => {
+    tar1.classList.add('visible')
 })
